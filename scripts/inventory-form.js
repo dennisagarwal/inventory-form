766dee1de38f79233e9be7c4cd56e4bd828f8f18
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-12-17 12:25:40 -0500

    form-styling

--- scripts/inventory-form.js
+++ scripts/inventory-form.js
@@ -1,23 +1,23 @@
 console.log("hello")
-n =  new Date();
-y = n.getFullYear();
-m = n.getMonth() + 1;
-d = n.getDate();
-document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+
 function SubForm (){
   $.ajax({
     url:'https://api.apispreadsheets.com/data/Buvk4PaGh1x69ysd/',
     //type post to post the data
     type:'post',
+    //serialization array converts to object
     data:$("#inventoryForm").serializeArray(),
+
 
     //success id called if the function call is successful otherwise error
     success: function(){
       alert("Form Data Submitted :)")
+      console.log($("#inventoryForm").serializeArray())
     },
     error: function(){
       alert("There was an error :(")
     }
+
   });
 
   }
